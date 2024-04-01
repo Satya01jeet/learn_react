c54ef81b42dfe41e72bd60736da0051d0e9cafee
Satya <sausatyajeet100@gmail.com>
2024-04-01 15:12:56 +0530

    Destructured how cra works

---
+++ 01basicreact/src/App.js
@@ -0,0 +1,13 @@
+import './App.css';
+import Youtube from './Youtube';
+
+function App() {
+  return (
+    <>
+      <h1>Basic Classic React App</h1>
+      <Youtube/>
+    </>
+  );
+}
+
+export default App;
---
+++ 01basicreact/src/Youtube.jsx
@@ -0,0 +1,6 @@
+function Youtube(){
+    return (
+        <h1>I study on youtube</h1>
+    )
+}
+export default Youtube
